33f62a534b1d5c8a6f67e8f4384f129678591292
TheDanniCraft <mail@thedannicraft.de>
2026-05-05 11:56:49 +0200

    ⬆️ Add rewrite dependencies and analytics config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import crypto from "node:crypto";
 import type { NextConfig } from "next";
+import { withPlausibleProxy } from "next-plausible";
+
+const plausibleSrc = "https://analytics.thedannicraft.de/js/pa-vjEfRmCPakwCirroPOV8h.js";
+const plausibleScriptName = process.env.NEXT_PUBLIC_PLAUSIBLE_SCRIPT_NAME ?? process.env.PLAUSIBLE_SCRIPT_NAME ?? `${crypto.randomInt(1000, 10000)}-${crypto.randomBytes(8).toString("hex")}`;
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "standalone",
 };
 
-export default nextConfig;
+export default withPlausibleProxy({
+	src: plausibleSrc,
+	scriptPath: `/js/${plausibleScriptName}.js`,
+})(nextConfig);
